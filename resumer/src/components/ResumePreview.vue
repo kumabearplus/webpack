a1e5e347896c26930d6d86759016e523f2bb9cf5
kumabearpluss <1229425590@qq.com>
2017-06-14 07:13:28 +0800

    step-5

---
+++ resumer/src/components/ResumePreview.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="resumePreview">
+    I am resumePreview
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ResumePreview'
+  }
+</script>
+
+<style>
+  #resumePreview{
+    flex-grow: 1;
+    margin-left: 16px;
+    background: #fff;
+  }
+</style>
