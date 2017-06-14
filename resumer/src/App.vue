222785a94ed3bd8c16ae80009f11aefbe4882068
kumabearpluss <1229425590@qq.com>
2017-06-14 14:24:47 +0800

    step-6

--- resumer/src/App.vue
+++ resumer/src/App.vue
@@ -18,6 +18,7 @@
   import Topbar from './components/Topbar'
   import ResumeEditor from './components/ResumeEditor'
   import ResumePreview from './components/ResumePreview'
+  import icons from './assets/icons'
 
   export default {
     name: 'app',
@@ -26,31 +27,39 @@
         text: '你好'
       }
     },
-    components: {Topbar, ResumeEditor, ResumePreview}
+    components: {Topbar, ResumeEditor, ResumePreview},
+    created () {
+      document.body.insertAdjacentHTML('afterbegin', icons) //
+    }
   }
 </script>
 
-<style>
+<style lang="scss">
   .page{
     height: 100vh;
     display: flex;
     flex-direction: column;
     background: #EAEBEC;
+    >main{
+      flex-grow: 1;  
+    }
+    >main{
+      min-width: 1024px;
+      max-width: 1440px;
+      margin-top: 16px;
+      margin-bottom: 16px;
+      display: flex;
+      justify-content: space-between;
+      padding: 0 16px;
+      width: 100%; /* 试试不加这句会怎样 */
+      align-self: center;
+    }
   }
-  .page>main{
-    flex-grow: 1;  
+  svg.icon{
+  height: 1em;
+  width: 1em;
+  fill: currentColor;
+  vertical-align: -0.1em;
+  font-size:16px;
   }
-
-  .page>main{
-    min-width: 1024px;
-    max-width: 1440px;
-    margin-top: 16px;
-    margin-bottom: 16px;
-    display: flex;
-    justify-content: space-between;
-    padding: 0 16px;
-    width: 100%; /* 试试不加这句会怎样 */
-    align-self: center;
-  }
-</style>
 </style>
